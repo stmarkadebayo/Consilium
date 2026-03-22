e03efaeca24615c9b806fe11e851b0d743434eba
Mark Adebayo <adebayomarkadedayo@gmail.com>
2026-03-22 12:05:00 +0100

    Add engines package exports

---
+++ backend/app/engines/__init__.py
@@ -0,0 +1,4 @@
+from app.engines.council_reasoning import CouncilReasoningEngine
+from app.engines.persona_intelligence import PersonaIntelligenceEngine
+
+__all__ = ["CouncilReasoningEngine", "PersonaIntelligenceEngine"]
